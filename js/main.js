bffcfbe319875faf06ef9b69fcd5874768ae2563
Fuller93 <141925702+Fuller93@users.noreply.github.com>
2025-07-24 20:27:01 +0100

    Bootstrap added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+const navBar = document.getElementById("navbar");
+
+window.toggleMenu = function (button) {
+    button.classList.toggle("is-active");
+
+    if (button.classList.contains("is-active")) {
+    navBar.style.right = "0";
+    } else {
+    navBar.style.right = "-250px";
+    }
+};
+});
